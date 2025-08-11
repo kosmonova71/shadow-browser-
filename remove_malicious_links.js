c8a32be1983a1023aaa5b755d0313a9489439ea9
ShadowyFigure <mrpowerup@hotmail.com>
2025-08-11 16:22:05 +1000

    Delete remove_malicious_links.js

--- remove_malicious_links.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    // Remove empty <a> tags with href="#" and target="_blank" and no meaningful content
-    document.querySelectorAll('a[target="_blank"][href="#"]').forEach(el => {
-        if (!el.textContent.trim()) {
-            el.remove();
-            console.log("Removed suspicious empty anchor tag:", el);
-        }
-    });
-})();
